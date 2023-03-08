31d041234610fb0d468dab92d66b5dcf5eb11866
Chris <c.bailey00@hotmail.com>
2023-03-08 20:12:20 +0000

    data stored in json

--- src/components/raceInfoBox.js
+++ src/components/raceInfoBox.js
@@ -1,32 +1,54 @@
-import React from "react"
+import { useState } from "react"
 
-function RaceInfoBox( {raceInfo}) {
+function RaceInfoBox({raceInfo}) {
 
-  const [showInfo, setShowInfo] = React.useState(false)
+  const [showInfo, setShowInfo] = useState(false)
 
   function timeFormat(time) {
     const date = new Date(time)
-    const hour = date.getHours()
+    const hour = '0' + date.getHours()
     const minutes = '0' + date.getMinutes()
 
-    return `${hour}:${minutes.substr(-2)}`
+    return `${hour.substr(-2)}:${minutes.substr(-2)}`
   }
 
   function handleClick() {
     setShowInfo(!showInfo)
   }
 
-  return(
-    <div className='race-info-box' onClick={handleClick}>
-        <h3 data-cy="race-title">{raceInfo.description}</h3>
-      {showInfo && <div className='race-info'>
-        <h4>{raceInfo.stages[3].description}</h4>
-        <p data-cy="quali-time">Start Time: {timeFormat(raceInfo.stages[3].scheduled)}</p>
-        <h4>{raceInfo.stages[4].description}</h4>
-        <p data-cy="race-time">Start Time: {timeFormat(raceInfo.stages[4].scheduled)}</p>
-      </div>}
-    </div>
-  )
+  // Check if weekend has a sprint race
+  if (raceInfo.stages[3].description === 'Sprint') {
+
+    return(
+      <div className='race-info-box' onClick={handleClick}>
+        <h3 data-cy="race-title">{raceInfo.description} - Sprint Week</h3>
+        {showInfo && <div className='race-info'>
+          <h4>{raceInfo.stages[1].description}</h4>
+          <p data-cy="quali-time">Start Time: {timeFormat(raceInfo.stages[1].scheduled)}</p>
+          <h4>{raceInfo.stages[3].description}</h4>
+          <p data-cy="sprint-time">Start Time: {timeFormat(raceInfo.stages[3].scheduled)}</p>
+          <h4>{raceInfo.stages[4].description}</h4>
+          <p data-cy="race-time">Start Time: {timeFormat(raceInfo.stages[4].scheduled)}</p>
+        </div>}
+      </div>
+    )
+  } else {
+
+    return(
+      <div className='race-info-box' onClick={handleClick}>
+          <h3 data-cy="race-title">{raceInfo.description}</h3>
+        {showInfo && <div className='race-info'>
+          <h4>{raceInfo.stages[3].description}</h4>
+          <p data-cy="quali-time">Start Time: {timeFormat(raceInfo.stages[3].scheduled)}</p>
+          <h4>{raceInfo.stages[4].description}</h4>
+          <p data-cy="race-time">Start Time: {timeFormat(raceInfo.stages[4].scheduled)}</p>
+        </div>}
+      </div>
+    )
+  }
+
+
+  
 }
 
 export default RaceInfoBox
