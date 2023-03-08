4799b24f40022420ab7b88f99ef09ded77664c20
Chris <c.bailey00@hotmail.com>
2023-03-08 20:43:33 +0000

    added test for home.js

--- src/components/raceInfoBox.js
+++ src/components/raceInfoBox.js
@@ -16,39 +16,37 @@ function RaceInfoBox({raceInfo}) {
     setShowInfo(!showInfo)
   }
 
-  // Check if weekend has a sprint race
-  if (raceInfo.stages[3].description === 'Sprint') {
-
-    return(
-      <div className='race-info-box' onClick={handleClick}>
-        <h3 data-cy="race-title">{raceInfo.description} - Sprint Week</h3>
-        {showInfo && <div className='race-info'>
+  // Check if weekend has sprint race or not
+  function sprintRace() {
+    if (raceInfo.stages[3].description === 'Sprint') {
+      return (
+        <>
           <h4>{raceInfo.stages[1].description}</h4>
           <p data-cy="quali-time">Start Time: {timeFormat(raceInfo.stages[1].scheduled)}</p>
           <h4>{raceInfo.stages[3].description}</h4>
           <p data-cy="sprint-time">Start Time: {timeFormat(raceInfo.stages[3].scheduled)}</p>
-          <h4>{raceInfo.stages[4].description}</h4>
-          <p data-cy="race-time">Start Time: {timeFormat(raceInfo.stages[4].scheduled)}</p>
-        </div>}
-      </div>
-    )
-  } else {
-
-    return(
-      <div className='race-info-box' onClick={handleClick}>
-          <h3 data-cy="race-title">{raceInfo.description}</h3>
-        {showInfo && <div className='race-info'>
+        </>
+      )
+    } else {
+      return (
+        <>
           <h4>{raceInfo.stages[3].description}</h4>
           <p data-cy="quali-time">Start Time: {timeFormat(raceInfo.stages[3].scheduled)}</p>
-          <h4>{raceInfo.stages[4].description}</h4>
-          <p data-cy="race-time">Start Time: {timeFormat(raceInfo.stages[4].scheduled)}</p>
-        </div>}
-      </div>
-    )
+        </>
+      )
+    }
   }
 
-
-  
+  return(
+    <div className='race-info-box' onClick={handleClick}>
+      <h3 data-cy="race-title">{raceInfo.description}</h3>
+      {showInfo && <div className='race-info'>
+        {sprintRace()}
+        <h4>{raceInfo.stages[4].description}</h4>
+        <p data-cy="race-time">Start Time: {timeFormat(raceInfo.stages[4].scheduled)}</p>
+      </div>}
+    </div>
+  )
 }
 
 export default RaceInfoBox
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,28 +2,6 @@ import data from '../raceInfo.json'
 import RaceInfoBox from "../components/raceInfoBox"
 
 function Home() {
-
-	// const raceInfo2 = {
-	// 	'description': 'Bahrain Grand Prix 2023',
-	// 	'stages': [{
-	// 		'description':'Practice 1'
-	// 	},
-	// 	{
-	// 		'description': 'Practice 2'
-	// 	},
-	// 	{
-	// 		'description': 'Practice 3'
-	// 	},
-	// 	{
-	// 		'description': 'Qualification',
-	// 		'scheduled': '2023-03-04T15:00:00+00:00'
-	// 	},
-	// 	{
-  //     'description': 'Race',
-  //     'scheduled': '2023-03-05T15:00:00+00:00'
-  //   }]
-	// }
-
 	const races = data.stages
 
 	return(
