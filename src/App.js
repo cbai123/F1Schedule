e3b7e8784a074421622afa478c36499463d11d14
Chris <c.bailey00@hotmail.com>
2023-03-09 21:14:00 +0000

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import Home from './pages/home'
+import Race from './pages/Race'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header className="App-header">
         <h2>F1 Schedule</h2>
       </header>
 
       <body className="App">
-        <Home />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path={'/:round'} element={<Race />} />
+          </Routes>
       </body>
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Race.js
@@ -0,0 +1,8 @@
+function Race() {
+
+  return(
+    <h1>Hello World!</h1>
+  )
+}
+
+export default Race
