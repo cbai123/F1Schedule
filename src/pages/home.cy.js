f893b292fc47012ef336cb9358572ee3fe86a045
Chris <c.bailey00@hotmail.com>
2023-03-22 14:57:33 +0000

    added testing for next race based on date

--- src/pages/home.cy.js
+++ src/pages/home.cy.js
@@ -4,11 +4,66 @@ import '../App.css'
 
 describe('<Home />', () => {
   beforeEach(() => {
+
+    cy.intercept({
+      method: 'GET',
+      url: 'https://ergast.com/api/f1/current.json'
+    },
+    {fixture: 'races.json'}
+    ).as('getRaces')
+
     cy.mount(<Home />)
+    cy.wait('@getRaces')
+  })
+  
+  it('renders the correct number of raceInfo boxes', () => {
+    cy.mount(<Home />)
+    cy.wait('@getRaces')
+
+    cy.get('.race-info-box').should('have.length', 2)
+    cy.get('.next-race-info-box').should('have.length', 1)
   })
 
-  it('renders the correct number of raceInfo boxes', () => {
-    cy.get('.race-info-box').should('have.length', 22)
-    cy.get('.next-race-info-box').should('have.length', 1)
+  afterEach(() => {
+    cy.clock().then((clock) => {
+      clock.restore()
+    })
+  })
+
+  it('returns the correct next race test 1', () => {
+    const date = new Date('2023/03/25')
+    cy.clock(date)
+    cy.mount(<Home />)
+    cy.wait('@getRaces')
+
+    cy.get('.next-race-info-box').should('contain.text', 'Australian Grand Prix')
+  })
+
+  it('returns the correct next race test 2', () => {
+    const date = new Date('2023/03/15')
+    cy.clock(date)
+    cy.mount(<Home />)
+    cy.wait('@getRaces')
+
+    cy.get('.next-race-info-box').should('contain.text', 'Saudi Arabian Grand Prix')
+  })
+
+  it('returns the correct next race if it is the day of the race', () => {
+    const date = new Date('2023/04/02')
+    cy.clock(date)
+    cy.mount(<Home />)
+    cy.wait('@getRaces')
+
+    cy.get('.next-race-info-box').should('contain.text', 'Australian Grand Prix')
+  })
+
+  it('should not return a next race if the date is past the last race', 
+  () => {
+    const date = new Date('2023/04/03')
+    cy.clock(date)
+    cy.mount(<Home />)
+    cy.wait('@getRaces')
+
+    cy.get('.next-race-info-box').should('not.exist')
   })
 })
