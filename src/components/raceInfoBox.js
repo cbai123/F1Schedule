80d4396beabaf02df6f92d92bcdfe3b21795a3b9
Chris <c.bailey00@hotmail.com>
2023-03-22 16:06:55 +0000

    added timeFormat function back in to account for daylight savings

--- src/components/raceInfoBox.js
+++ src/components/raceInfoBox.js
@@ -4,6 +4,15 @@ function RaceInfoBox({raceInfo, nextRace, navigate}) {
   const [showInfo, setShowInfo] = useState(false)
 
   const raceDate = new Date(raceInfo.date)
+
+  // This function needs to be here instead of slicing the time to account for BST
+  function timeFormat(date, time) {
+    const dateTime = new Date(`${date}T${time}`)
+    const hour = '0' + dateTime.getHours()
+    const minutes = '0' + dateTime.getMinutes()
+
+    return `${hour.substr(-2)}:${minutes.substr(-2)}`
+  }
   
   function dateFormat() {
     const day = '0' + raceDate.getDate()
@@ -34,15 +43,15 @@ function RaceInfoBox({raceInfo, nextRace, navigate}) {
       <h3 className="race-title">{dateFormat()}&emsp;{raceInfo.raceName}</h3>
       {showInfo && <div className='race-info'>
         <h4 className="info-box-quali">Qualifying</h4>
-        <p className="info-box-time" data-cy="quali-time">Start Time: {raceInfo.Qualifying.time.slice(0,5)}</p>
+        <p className="info-box-time" data-cy="quali-time">Start Time: {timeFormat(raceInfo.Qualifying.date, raceInfo.Qualifying.time)}</p>
         {raceInfo.Sprint && 
         <>
           <h4 className="info-box-sprint">Sprint</h4>
-          <p className="info-box-time" data-cy="sprint-time">Start Time: {raceInfo.Sprint.time.slice(0,5)}</p>
+          <p className="info-box-time" data-cy="sprint-time">Start Time: {timeFormat(raceInfo.Sprint.date, raceInfo.Sprint.time)}</p>
         </>
         }
         <h4 className="info-box-race">Race</h4>
-        <p className="info-box-time" data-cy="race-time">Start Time: {raceInfo.time.slice(0,5)}</p>
+        <p className="info-box-time" data-cy="race-time">Start Time: {timeFormat(raceInfo.date, raceInfo.time)}</p>
         {raceDate <= new Date() && <button className='more-info-btn' onClick={changePage}>Results</button>}
       </div>}
     </div>
