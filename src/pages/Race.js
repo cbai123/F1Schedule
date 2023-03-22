2723085b16f755de841133de8f0519ffe390df41
Chris <c.bailey00@hotmail.com>
2023-03-22 15:43:08 +0000

    added results to each races more info page

--- src/pages/Race.js
+++ src/pages/Race.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 function Race() {
+  const [resultsArr, setResultsArr] = useState([])
+  const { round } = useParams()
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      fetch(`https://ergast.com/api/f1/current/${round}/results.json`)
+      .then(response => response.json())
+      .then(result => result.MRData.RaceTable.Races[0].Results)
+      .then(data => setResultsArr(data))
+    }
+
+    fetchResults()
+  })
 
   return(
     <>
@@ -10,7 +26,12 @@ function Race() {
         <div>
           <h1 style={{margin: 0}}>In Progress :)</h1>
         </div>
-        
+        <div>
+          <h3>Results: </h3>
+          {resultsArr.map((result, index) => {
+            return <p key={index}>{result.position}: {result.Driver.givenName} {result.Driver.familyName}</p>
+          })}
+        </div>
       </body>
 		</>
   )
